b37c8dc32cfd4620166edfc786db61c468112e8e
H-ROD-10 <hector.rodriguezb@gmail.com>
2022-07-31 12:15:12 -0400

    Proyecto inicial listo

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Navigation } from './routes/Navigation';
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <Navigation/>
+      </div>
+    </>
+  )
+}
+
+export default App
